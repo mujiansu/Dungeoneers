a8cd7460ce65509826773f3ce5d169e559864926
renzoyzz <renzoyzevallos@gmail.com>
2020-11-30 13:02:32 -0800

    Configure DI. Add Steam Helpers

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class GameManager : MonoBehaviour
+{
+
+    private SteamManager _steamManager;
+    [Inject]
+    public void Construct(SteamManager steamManager)
+    {
+        _steamManager = steamManager;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(_steamManager);
+    }
+
+    private void Update()
+    {
+        _steamManager.RunCallbacks();
+    }
+}
---
+++ Assets/Scripts/Steamworks.NET/SteamHelpers.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using MessagePack;
+using Steamworks;
+using UnityEngine;
+
+public class SteamHelpers
+{
+    private static readonly Dictionary<Type, object> Callbacks = new Dictionary<Type, object>();
+
+    public static CSteamID Me;
+
+    /// <summary>
+    /// Register callback to steam
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="func"></param>
+    public static void RegisterCallback<T>(Callback<T>.DispatchDelegate func)
+    {
+        Callbacks.Add(typeof(T), Callback<T>.Create(func));
+    }
+
+    public static void Init()
+    {
+        Me = SteamUser.GetSteamID();
+    }
+
+    public static bool GetPacket<T>(out T packet, out CSteamID remoteId, PacketChannel channel)
+    {
+        if (SteamNetworking.IsP2PPacketAvailable(out var packetSize, (int)channel))
+        {
+            var packetBytes = new byte[packetSize];
+            if (!SteamNetworking.ReadP2PPacket(packetBytes, packetSize, out var bytesRead, out var packetRemoteId, (int)channel) || packetRemoteId == Me)
+            {
+                packet = default(T);
+                remoteId = packetRemoteId;
+                return false;
+            }
+            packet = MessagePackSerializer.Deserialize<T>(packetBytes);
+            remoteId = packetRemoteId;
+            return true;
+        }
+        packet = default(T);
+        remoteId = default(CSteamID);
+        return false;
+    }
+
+    public enum PacketChannel
+    {
+    }
+}
