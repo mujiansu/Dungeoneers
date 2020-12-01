613944f96a7ec53c5f751af3850905f3649ccdcd
renzoyzz <renzoyzevallos@gmail.com>
2020-12-01 00:31:35 -0800

    Create Friend List.

---
+++ Assets/Scripts/UI/StartMenu/FriendElement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FriendElement : MonoBehaviour
+{
+    [SerializeField]
+    private Image _imgAvatar;
+    [SerializeField]
+    private Text _txtUsername;
+    [SerializeField]
+    private Text _txtGameState;
+
+    public void SetFriendMetadata(SteamFriendMetadata metadata)
+    {
+        _imgAvatar.sprite = Sprite.Create(metadata.Avatar, _imgAvatar.rectTransform.rect, Vector2.zero);
+        _txtUsername.text = metadata.Name;
+        _txtGameState.text = metadata.GameId.ToString();
+    }
+}
