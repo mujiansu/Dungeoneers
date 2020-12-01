a738f45396bd117bff78600369747b820a991f0f
renzoyzz <renzoyzevallos@gmail.com>
2020-11-30 16:04:04 -0800

    Add character animation

--- Assets/Scripts/Player/Characters/Character.cs
+++ Assets/Scripts/Player/Characters/Character.cs
@@ -1,18 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Character : MonoBehaviour
 {
+
+    private Animator _animator;
     // Start is called before the first frame update
     void Start()
     {
-
+        _animator = GetComponent<Animator>();
+        _animator.Play("Test_Idle");
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        _animator.Play("Test_Running");
+        _animator.SetFloat("x_vel", .9f);
+        _animator.SetFloat("y_vel", -1f);
     }
 }
