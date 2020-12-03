0458de2a3359bf828f61183825165447fe3c8ea7
Garren <garrenboggs@gmail.com>
2020-12-02 19:12:49 -0500

    GDB: Added Tilemap, and create scene manager

---
+++ Assets/Scripts/Scene/SceneController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    
+    public delegate void SceneLoadDelegate(SceneType _scene);
+    public static SceneController instance;
+    
+
+    //private PageController _Menu;
+    private SceneType _targetScene;
+    private SceneLoadDelegate _sceneLoadDelegate;
+    private bool _sceneIsLoading;
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+#region Unity Functions
+    private void Awake() 
+    {
+        
+
+    }
+
+    private void OnDisable() 
+    {
+        
+    }
+#endregion
+
+#region Public Functions
+    public void Load(SceneType _scene, 
+        SceneLoadDelegate _sceneLoadDelegate=null,
+        bool _reload=false,
+        PageType _loadingPage=PageType.None)
+    {
+        
+    }
+#endregion
+
+#region Private Functions
+    private void Configure()
+    {
+
+    }
+
+    private void Dispose()
+    {
+
+    }
+
+    prvate async void 
+
+
+
+#endregion
+
+}
---
+++ Assets/Scripts/Scene/TestScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScene : MonoBehaviour
+{
+
+    private PlayerInput _playerInput;
+    public SceneController sceneController;
+
+    void Start()
+    {
+        _playerInput = GetComponent<PlayerInput>();
+    }
+
+    void Update()
+    {
+        if(_playerInput.Keystroke.ReadValue<float>()>0f)
+        {
+            
+        }
+    }
+}
