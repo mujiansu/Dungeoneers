e889ec627709b85f209ef5c7a4d4cd101990d6eb
renzoyzz <renzoyzevallos@gmail.com>
2020-12-03 22:38:35 -0800

    Created packet formatter

--- Assets/Scripts/Startup.cs
+++ Assets/Scripts/Startup.cs
@@ -3,25 +3,38 @@ using System.Collections.Generic;
 using MessagePack;
 using MessagePack.Resolvers;
 using MessagePack.Unity;
+using MessagePack.Unity.Extension;
 using UnityEngine;
 
 public class Startup
 {
-    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterAssembliesLoaded)]
+    private static bool _initialized = false;
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     static void Initialize()
     {
 
-        StaticCompositeResolver.Instance.Register(
-            MessagePack.Resolvers.GeneratedResolver.Instance,
-            UnityResolver.Instance,
-            MessagePack.Unity.Extension.UnityBlitWithPrimitiveArrayResolver.Instance,
-            StandardResolver.Instance
-        );
-        var option = MessagePackSerializerOptions.Standard.WithResolver(StaticCompositeResolver.Instance);
+        if (!_initialized)
+        {
+            StaticCompositeResolver.Instance.Register(
+                        UnityResolver.Instance,
+                        UnityBlitWithPrimitiveArrayResolver.Instance,
+                        StandardResolver.Instance,
+                        PacketResolver.Instance
+                    );
+            var options = MessagePackSerializerOptions.Standard.WithResolver(StaticCompositeResolver.Instance);
+            MessagePackSerializer.DefaultOptions = options;
 
-        MessagePack.MessagePackSerializer.Serialize(new CharacterPacket());
+            var packet = MessagePack.MessagePackSerializer.Serialize(new CharacterPacket
+            {
+                Pos = new Vector2(2.1f, 3.5f)
+            });
+            var packet1 = MessagePack.MessagePackSerializer.Deserialize<CharacterPacket>(packet);
+            _initialized = true;
+        }
 
     }
+
+
 #if UNITY_EDITOR
 
 
