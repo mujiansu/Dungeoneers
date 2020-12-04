c3b8d9eb549c63c6cab57dbd7c7fd49764d58810
renzoyzz <renzoyzevallos@gmail.com>
2020-12-03 22:38:49 -0800

    removed test serializer

--- Assets/Scripts/Startup.cs
+++ Assets/Scripts/Startup.cs
@@ -23,12 +23,6 @@ public class Startup
                     );
             var options = MessagePackSerializerOptions.Standard.WithResolver(StaticCompositeResolver.Instance);
             MessagePackSerializer.DefaultOptions = options;
-
-            var packet = MessagePack.MessagePackSerializer.Serialize(new CharacterPacket
-            {
-                Pos = new Vector2(2.1f, 3.5f)
-            });
-            var packet1 = MessagePack.MessagePackSerializer.Deserialize<CharacterPacket>(packet);
             _initialized = true;
         }
 
